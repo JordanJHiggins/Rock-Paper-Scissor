51e5a1ae1268bc556de761c49a9db5b2b0ab38ae
Jordanjhiggins <jordan.higgins14@gmail.com>
2022-09-24 17:00:14 -0300

    fix game logic to play first to 5 points

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let choices = ["Rock", "Paper", "Scissors"];
+let choices = ["rock", "paper", "scissors"];
 
 // Random computer selection
 function getComputerChoice() {
@@ -6,19 +6,19 @@ function getComputerChoice() {
   return randomChoice;
 }
 
-// Play a single round between comp and human player
+// Play a single round between comp and human player (console.logs will become returns when GUI is added)
 function playRound(computerSelection, playerSelection) {
   if (computerSelection === playerSelection) {
     console.log(
       `It's a tie! Computer chose ${computerSelection} and you chose ${playerSelection}`
     );
-  } else if (computerSelection === "Rock" && playerSelection === "Scissors") {
+  } else if (computerSelection === "rock" && playerSelection === "scissors") {
     console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
     computerScore++;
-  } else if (computerSelection === "Paper" && playerSelection === "Rock") {
+  } else if (computerSelection === "paper" && playerSelection === "rock") {
     console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
     computerScore++;
-  } else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+  } else if (computerSelection === "scissors" && playerSelection === "paper") {
     `You lose! ${computerSelection} beats ${playerSelection}`;
     computerScore++;
   } else {
@@ -31,22 +31,24 @@ function playRound(computerSelection, playerSelection) {
 let playerScore = 0;
 let computerScore = 0;
 
-// Initialize a game of 5 rounds
+// Initialize a game of 5 rounds (console.logs will become returns when GUI is added)
 function game() {
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 20; i++) {
     let playerSelection = window.prompt("Make a selection: ").toLowerCase();
     let computerSelection = getComputerChoice().toLowerCase();
-    playRound(computerSelection, playerSelection);
-  }
 
-  if (playerScore > computerScore) {
-    console.log(
-      `Gameover! you win Player: ${playerScore} Computer: ${computerScore}`
-    );
-  } else if (computerScore > playerScore) {
-    console.log(
-      `Gameover! You lose Computer: ${computerScore} Player: ${playerScore}`
-    );
+    if (playerScore === 5) {
+      console.log(
+        `Gameover! you win Player: ${playerScore} Computer: ${computerScore}`
+      );
+      return;
+    } else if (computerScore === 5) {
+      console.log(
+        `Gameover! You lose Computer: ${computerScore} Player: ${playerScore}`
+      );
+      return;
+    }
+    playRound(computerSelection, playerSelection);
   }
 }
 
